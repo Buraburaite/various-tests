88cc9225d454d0a5cc4e0f5209a7b835265f10f1
Javier <jvrcueto@outlook.com>
2017-06-03 22:55:28 -0700

    Revert "added jQuery CDN, made many small changes (package.json, public folder, index.ejs, etc." This reverts commit 8958f55208bef46c36a64537da13070a5422cb31.
    Forgot to create a .gitignore for the node_modules folder

--- browser-environment/app.js
+++ browser-environment/app.js
@@ -1,5 +1,6 @@
 const express      = require('express');
 const path         = require('path');
+const favicon      = require('serve-favicon');
 const logger       = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser   = require('body-parser');
@@ -18,6 +19,8 @@ app.set('view engine', 'ejs');
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
 
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
--- browser-environment/public/javascripts/docFragTest.js
+++
@@ -1,7 +0,0 @@
-console.log(window);
-
-const $window = $(window);
-const $document = $(document);
-
-let frag = document.createDocumentFragment();
-console.log(frag);
